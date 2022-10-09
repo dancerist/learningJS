05fbdb615d6283d60ee8b39674d7482608a222ed
Dancerist <nlanugon29@gmail.com>
2022-10-09 09:48:35 +0800

    New commit message

---
+++ script.js
@@ -0,0 +1,17 @@
+function print(firstName) {
+    console.log("Hello " + firstName);
+
+}
+
+print("Isabelle");
+
+function createEmail(firstName, price) {
+    let shipping = 5.95;
+    console.log(`Hi ${firstName}! Thnanks!
+    Total: $${price}
+    Shipping: $${shipping}
+    Grand Total: $${price + shipping}
+    `);
+}
+
+createEmail("Guy", 100)
